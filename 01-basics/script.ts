c7766a075c47c4551ce0d47f3670425812b569cb
Johan Nordström <johan@digitalvillage.se>
2024-09-09 21:27:10 +0200

    Our first lines of TypeScript 🤩.

---
+++ 01-basics/script.ts
@@ -0,0 +1,27 @@
+export {};
+
+/*
+let myName: string;
+myName = "Johan";
+myName = "Pelle";
+// myName = 1337;
+myName.toUpperCase();
+
+let myNum = 1337;
+// myNum = false;
+myNum.toFixed();
+*/
+
+/*
+// Explicit typing 😕
+let myName: string = "Johan";
+let myAge: number = 42;
+let needsMoreCoffee: boolean = true;
+*/
+
+// Implicit typing 🤩
+let myName = "Johan";		// Typen blir automatiskt string
+let myAge = 42;				// Typen blir automatiskt number
+let needsMoreCoffee = true;	// Typen blir automatiskt boolean
+
+// needsMoreCoffee = null;		// Type 'null' is not assignable to type 'boolean'
