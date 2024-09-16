ed2caf78808c80f086c5b185939e407393c8cd6f
Johan Nordström <johan@digitalvillage.se>
2024-09-16 20:04:39 +0200

    Fun times with Types vs Interfaces 🤩.

--- 02-vite-basics/src/main.ts
+++ 02-vite-basics/src/main.ts
@@ -55,6 +55,7 @@ console.log(ages);
 // 	console.log(`x=${point.x}, y=${point.y}`);
 // }
 
+// Type Alias
 type Point = {
 	x: number;
 	y: number;
@@ -79,3 +80,97 @@ const myCoords1 = { x: 42, y: 1337 };
 printCoords(myPoint);
 printCoords(myCoords1);
 printCoords( { x: 42, y: 1337, z: -1 } );
+
+/**
+ * Type Aliases
+ */
+
+// Type Aliases can be assigned primitive types
+type Nummer = number;
+let x: Nummer = 42;
+let y: number = 1337;
+const sumNum = (a: Nummer, b: number) => { return a + b; }
+sumNum(y, x);
+
+// Type Aliases can also be a union between two (or more) types
+type StringOrNumber = string | number;
+let s: StringOrNumber = 42;
+s = "forty-two";
+
+/**
+ * Makes a dull message more interesting
+ *
+ * @param msg Dull message
+ */
+const makeMoreInteresting = (msg: StringOrNumber) => {
+	return (typeof msg === "string")
+		? msg.toUpperCase() + "!!!!11"
+		: String(msg) + "!!!!11";
+}
+console.log(makeMoreInteresting("lolcats are funny"));
+console.log(makeMoreInteresting(1337));
+// console.log(makeMoreInteresting(["loldogs"]));  // not workey
+
+/**
+ * Interfaces
+ *
+ * Mainly (or almost exclusively) used to describe an object
+ */
+
+interface IPoint {
+	x: number;
+	y: number;
+	z?: StringOrNumber;
+}
+
+// Inheritance 💰
+interface IAnimal {
+	name: string;
+}
+
+interface IDog extends IAnimal {
+	legs: number;
+}
+
+// Interface Merging - This will be merged with the above interface **WITHOUT** any conflicts
+interface IDog extends IAnimal {
+	wagsTail: boolean;
+}
+
+interface ISnake extends IAnimal {
+	sneaky: boolean;
+}
+
+const doge: IDog = {
+	name: "Doge",
+	legs: 4,
+	wagsTail: true,
+}
+
+// We can also do (almost) the same with Type Aliases
+type Animal = {
+	name: string;
+}
+
+// type Animal = {  // nope
+// 	species: string;
+// }
+
+// Type Intersection
+type Dog = Animal & {
+	legs: number;
+}
+
+const doge2: Dog = {
+	name: "Doge Jr",
+	legs: 4,
+}
+
+
+type Todo = {
+	title: string;
+	completed: boolean;
+}
+
+// Not possible with an interface
+type TodoList = Todo[];
