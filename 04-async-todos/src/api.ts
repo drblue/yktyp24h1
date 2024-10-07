089e17af6b57f87cc71460dbfd3c2f7d93d108b9
Johan Nordström <johan@digitalvillage.se>
2024-10-07 18:28:20 +0200

    Use a axios instance instead.

--- 04-async-todos/src/api.ts
+++ 04-async-todos/src/api.ts
@@ -4,13 +4,23 @@
 import axios from "axios";
 import type { CreateTodoPayload, Todo, UpdateTodoPayload } from "./api.types";
 
-const baseUrl = import.meta.env.VITE_API_BASEURL as string || "http://localhost:3000";
+const baseURL = import.meta.env.VITE_API_BASEURL as string || "http://localhost:3000";
+
+// Create a new axios instance
+const instance = axios.create({
+	baseURL,
+	headers: {
+		"Accept": "application/json",
+		"Content-Type": "application/json",
+	},
+	timeout: 10000,
+});
 
 /**
  * Get todos from API using fetch
  */
 export const getTodosFetch = async () => {
-	const response = await fetch(baseUrl + "/todos");    // http://localhost:3000/todos
+	const response = await fetch(baseURL + "/todos");    // http://localhost:3000/todos
 //         ^?
 
 	if (!response.ok) {
@@ -31,7 +41,7 @@ export const getTodosFetch = async () => {
  * Get todos from API using axios
  */
 export const getTodos = async () => {
-	const response = await axios.get<Todo[]>(baseUrl + "/todos");
+	const response = await instance.get<Todo[]>("/todos");
 
 	return response.data;
 }
@@ -41,7 +51,7 @@ export const getTodos = async () => {
  */
 export const createTodo = async (data: CreateTodoPayload) => {
 	// Send a POST-request to http://localhost:3000/todos with the contents of `todo` as body
-	const response = await axios.post<Todo>(baseUrl + "/todos", data);
+	const response = await instance.post<Todo>("/todos", data);
 	return response.data;
 }
 
@@ -50,7 +60,7 @@ export const createTodo = async (data: CreateTodoPayload) => {
  */
 export const updateTodo = async (id: number, data: UpdateTodoPayload) => {
 	// Send a PATCH-request to http://localhost:3000/todos/:id with the contents of `data` as body
-	const response = await axios.patch<Todo>(baseUrl + "/todos/" + id, data);
+	const response = await instance.patch<Todo>("/todos/" + id, data);
 	return response.data;
 }
 
@@ -59,6 +69,6 @@ export const updateTodo = async (id: number, data: UpdateTodoPayload) => {
  */
 export const deleteTodo = async (id: number) => {
 	// Send a DELETE-request to http://localhost:3000/todos/:id
-	await axios.delete(baseUrl + "/todos/" + id);
+	await instance.delete("/todos/" + id);
 	return true;
 }
