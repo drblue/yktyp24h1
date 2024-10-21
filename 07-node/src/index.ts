6887553fc8501adf0c01f79895d9472f53a12d45
Johan Nordström <johan@digitalvillage.se>
2024-10-21 19:00:59 +0200

    Installed `ts-node` and `nodemon` to simplify our lives 🤩.

--- 07-node/src/index.ts
+++ 07-node/src/index.ts
@@ -1,8 +1,9 @@
 import { writeFile, appendFile } from "node:fs/promises";
 
-const msg: string = "Hello from the backend with types";
+const msg: string = "Hello from the backend with types and watches!";
 
 console.log(msg);
 console.log("Hello from the otter side 🦦");
+console.log("I NEEEDD COVFEFEFE!!!!!!!!!!!!!!!!!!!!!!!!!");
 
 appendFile("lolcats.txt", "are funny since " + Date.now() + "\n");
